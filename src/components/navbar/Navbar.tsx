a709cee9b1e1c2c38b48848b28efb2c7c608cdca
vietvanba <baviet19@gmail.com>
2024-03-23 00:11:27 +0700

    add drop-menu for navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,17 +3,32 @@ import menus from "./navbar.json";
 import { Link, NavLink } from "react-router-dom";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [title, setTitle] = useState();
+
+  const handleMouseEnter = (title: any) => {
+    setIsOpen(true);
+    setTitle(title);
+  };
+
+  const handleMouseLeave = (title: any) => {
+    setIsOpen(false);
+    setTitle(title);
+  };
   return (
     <div className="navbar">
       <Link to={"/"} className="homepage">
-        <img src="vite.svg" alt="" className="logo" />
+        <img src="logo.svg" alt="" className="logo" />
         <div className="logo-text">VanBaViet</div>
       </Link>
       <ul className="items">
         {menus.map((menu) => (
           <li className="item">
             <NavLink
+              onMouseEnter={() => handleMouseEnter(menu.name)}
+              onMouseLeave={() => handleMouseLeave(menu.name)}
               to={menu.url}
               className={({ isActive, isPending }) =>
                 isPending ? "pending" : isActive ? "active" : ""
@@ -21,6 +36,19 @@ export const Navbar = () => {
             >
               {menu.name}
             </NavLink>
+            {isOpen && title === menu.name && menu.items.length != 0 && (
+              <div
+                className="sub"
+                onMouseEnter={() => handleMouseEnter(menu.name)}
+                onMouseLeave={() => handleMouseLeave(menu.name)}
+              >
+                {menu.items.map((subItem, index) => (
+                  <Link className="subItem" key={index} to={subItem.url}>
+                    {subItem.name}
+                  </Link>
+                ))}
+              </div>
+            )}
           </li>
         ))}
       </ul>
